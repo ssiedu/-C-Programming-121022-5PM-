3d7674b0963a88703d86f58fb08e35c07525d619
ankitaSsi <104835257+ankitaSsi@users.noreply.github.com>
2022-11-01 19:53:26 +0530

    program

---
+++ datatype.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+void main()
+{
+	printf("\n Size of integer : %d",sizeof(int));
+	printf("\n size of float   : %d",sizeof(float));
+	printf("\n Size of character : %d",sizeof(char));
+	printf("\n size of long int : %d",sizeof(long int));
+	printf("\n size of double : %d",sizeof(double));
+	getch();
+}
